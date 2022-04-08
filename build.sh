88ce1fb2749d5c7048f2f1622889173bf5c40dbf
Maverick Leroux <maya-10@hotmail.ca>
2022-04-08 17:14:01 +0000

    Fix VDSO for clang compile-v4.14.248

--- build.sh
+++ build.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-KERNEL_DEFCONFIG=nova_defconfig
+KERNEL_DEFCONFIG=a71_eur_open_defconfig
 
 echo
 echo "==> Clean Build Directory"
-echo 
-
-make clean && make mrproper
-make O=out clean
+echo
 
 echo
 echo "==> Issue Build Commands"
 echo
 
 mkdir -p out
-export HOME=/home/phennaux
+export HOME=/workspace/samsung_android_kernel_sm6150
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=$HOME/Documents/kernel_dev/toolchains/azure-clang/bin
+export CLANG_PATH=$HOME/azure-clang/bin
 export PATH="$CLANG_PATH:$PATH"
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-export KBUILD_BUILD_USER=OneUI
-export KBUILD_BUILD_HOST=Nova
+export KBUILD_BUILD_USER=MasterKernelv2
+export KBUILD_BUILD_HOST=OVH
+KERNEL_MAKE_ENV="DTC_EXT=$HOME/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
 START=$(date +"%s")
 
@@ -30,7 +28,7 @@ echo
 echo "==> Kernel is using the current defconfig: $KERNEL_DEFCONFIG"
 echo
  
-make CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out $KERNEL_DEFCONFIG
+make CC=clang LD=ld.lld $KERNEL_MAKE_ENV AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out $KERNEL_DEFCONFIG
 
 echo
 echo "==> Begin compilation..."
